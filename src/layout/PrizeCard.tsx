6d69413a8090f853fe2f9c8e4510125b90337078
Данила Гринберг <dannniska@gmail.com>
2025-09-09 12:49:39 +0300

    fix card styles

--- src/layout/PrizeCard.tsx
+++ src/layout/PrizeCard.tsx
@@ -13,14 +13,14 @@ export const PrizeCard = forwardRef<HTMLDivElement, PrizeCardProps>(
 			<div
 				ref={ref}
 				className={classNames(
-					'flex-shrink-0 w-45 bg-white rounded-2xl shadow-md text-[#234B89]',
+					'flex-shrink-0 flex flex-col w-45 p-4 pb-7 bg-white rounded-3xl text-[#234B89]',
 					className
 				)}
 			>
-				<div className="size-45 flex justify-center items-center">
+				<div className="size-42 flex justify-center items-center">
 					<img className="mb-2 max-w-full max-h-full object-cover" src={imageUrl} alt="Prize" />
 				</div>
-				<p className="p-4 pb-7 text-left text-[#21201F] font-medium text-[16px] leading-[17px] tracking-[-0.08px]">
+				<p className="mt-auto text-left text-[#21201F] font-medium text-[16px] leading-[17px] tracking-[-0.08px]">
 					{title}
 				</p>
 			</div>
